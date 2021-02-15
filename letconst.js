c2c696075381ed8aaec728c04f575fe00612a839
dcduncanson <dcduncanson@gmail.com>
2021-02-14 23:17:25 -0700

    Unit 10 let const exercise

---
+++ letconst.js
@@ -0,0 +1,19 @@
+const PI = 3.14;
+PI = 42; // This will error because we made PI a constant.
+
+// What is the difference between var and let?
+// var is older and should not be used. let is block scoped. 
+// var can be redeclared and let can not. var can be hoisted.
+
+// What is the difference between var and const?
+// // var is older and should not be used. const is block scoped. 
+// // var can be redeclared and reassigned, const can not do either. var can be hoisted.
+
+// What is the difference between let and const?
+// let can be reassigned, const can't be reassigned.
+
+// What is hoisting?
+// Hoisting means that when you delcare a var, it will see the var declaration first when 
+// compiled. It will show as undefined if it is called before it is assigned a value.
+
+
